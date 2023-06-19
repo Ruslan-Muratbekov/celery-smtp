ca05a9d215cd91888e38aedeef7992334e53de01
Ruslan-Muratbekov <125174895+Ruslan-Muratbekov@users.noreply.github.com>
2023-06-19 12:47:31 +0600

    Update settings.py

--- send_email/settings.py
+++ send_email/settings.py
@@ -123,16 +123,16 @@ STATIC_URL = '/static/'
 # EMAIL_USE_USER = 'testtesttesttesttesttes0@gmail.com'
 # EMAIL_HOST_PASSWORD = 'dbvkmvykmvfzkbbt'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'testtesttesttesttesttes0@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbvkmvykmvfzkbbt'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
+EMAIL_BACKEND = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 
+EMAIL_USE_TLS = 
+DEFAULT_FROM_EMAIL = ''
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+DEFAULT_AUTO_FIELD = ''
+CELERY_BROKER_URL = ''
+CELERY_RESULT_BACKEND = ''
 
 # 'dbvkmvykmvfzkbbt'
